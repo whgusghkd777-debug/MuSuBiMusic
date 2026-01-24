964647168f0fdd636219c4433f2aca51fb7668aa
Bassist_Cho <whgusghkd777@gmail.com>
2026-01-24 21:17:52 +0900

    Update main.js

--- Musubi/src/main/resources/static/js/main.js
+++ Musubi/src/main/resources/static/js/main.js
@@ -1,11 +1,11 @@
-// 예시: 삭제 버튼 클릭 시
+
 async function deleteMusic(id) {
     if(!confirm("本当に削除しますか？")) return;
     
     const response = await fetch(`/api/music/delete/${id}`, {
         method: 'DELETE',
         headers: {
-            [csrfHeader]: csrfToken // 보안용 토큰 필수!
+            [csrfHeader]: csrfToken 
         }
     });
 
@@ -13,6 +13,6 @@ async function deleteMusic(id) {
         alert("削除に成功しました。");
         location.reload();
     } else {
-        alert("権限がありません。(admin 계정으로 로그인하세요)");
+        alert("権限がありません。(admin しかできません。)");
     }
 }
