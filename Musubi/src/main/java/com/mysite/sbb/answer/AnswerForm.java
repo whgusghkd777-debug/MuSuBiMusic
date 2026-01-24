b23a1c07581ce74b5432bed32302b01cb25cb8ba
Bassist_Cho <whgusghkd777@gmail.com>
2026-01-24 21:14:44 +0900

    Update AnswerForm.java

--- Musubi/src/main/java/com/mysite/sbb/answer/AnswerForm.java
+++ Musubi/src/main/java/com/mysite/sbb/answer/AnswerForm.java
@@ -8,6 +8,6 @@ import lombok.Setter;
 @Setter
 public class AnswerForm {
 
-    @NotEmpty(message = "内容は必須項目です。")  // 일본어 에러 메시지 (원하면 한국어로 바꿔)
+    @NotEmpty(message = "内容は必須項目です。") 
     private String content;
 }
